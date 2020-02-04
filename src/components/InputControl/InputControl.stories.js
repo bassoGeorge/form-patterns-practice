074729f4819c226339ba36259b974aad8d165f0e
Anish George <anishgeo@thoughtworks.com>
2020-02-04 17:39:19 +0530

    Write stories for the InputControl component

---
+++ src/components/InputControl/InputControl.stories.js
@@ -0,0 +1,136 @@
+import React, { useState } from "react";
+import { text, withKnobs, optionsKnob } from "@storybook/addon-knobs";
+import InputControl from "./InputControl";
+
+export default {
+  title: "Input Control",
+  component: InputControl,
+  decorators: [withKnobs]
+};
+
+function Statefully({ render }) {
+  const [value, setValue] = useState("");
+  return <>{render(value, setValue)}</>;
+}
+
+export const Basic = () => {
+  return (
+    <Statefully
+      render={(value, setValue) => (
+        <InputControl
+          value={value}
+          label={text("Label", "User Name")}
+          onChange={setValue}
+        />
+      )}
+    />
+  );
+};
+Basic.story = {
+  name: "Basic Usage"
+};
+
+export const WithHint = () => {
+  return (
+    <Statefully
+      render={(value, setValue) => (
+        <InputControl
+          label={text("Label", "Has Hint")}
+          value={value}
+          onChange={setValue}
+          hint={text(
+            "Hint",
+            "This para provides additional hint for the input"
+          )}
+        />
+      )}
+    />
+  );
+};
+
+WithHint.story = {
+  name: "Input with hint text"
+};
+
+export const WithError = () => {
+  return (
+    <Statefully
+      render={(value, setValue) => {
+        return (
+          <InputControl
+            label={"Error in input"}
+            value={value}
+            onChange={setValue}
+            hint={
+              "Error is achieved by passing a string to error prop. Falsy values remove the error"
+            }
+            error={text("Error", "Oh snap!")}
+          />
+        );
+      }}
+    />
+  );
+};
+
+WithError.story = {
+  name: "Input in error state"
+};
+
+export const TypeControl = () => {
+  const label = "Type";
+  const valuesObj = {
+    text: "text",
+    number: "number",
+    password: "password",
+    tel: "tel",
+    email: "email",
+    date: "date"
+  };
+  const defaultValue = "text";
+  const optionsObj = {
+    display: "radio"
+  };
+  const typeKnob = optionsKnob(label, valuesObj, defaultValue, optionsObj);
+
+  return (
+    <Statefully
+      render={(value, setValue) => {
+        return (
+          <InputControl
+            label={"Controlling the Type"}
+            value={value}
+            onChange={setValue}
+            hint={"Inputs can have their types controlled via the type prop"}
+            type={typeKnob}
+          />
+        );
+      }}
+    />
+  );
+};
+
+TypeControl.story = {
+  name: "Controlling the type of the input"
+};
+
+export const PasswordField = () => {
+  return (
+    <Statefully
+      render={(value, setValue) => {
+        return (
+          <InputControl
+            label="Password field behaviour"
+            value={value}
+            onChange={setValue}
+            hint="Password fields come with a show password button in built"
+            type="password"
+          />
+        );
+      }}
+    />
+  );
+};
+
+PasswordField.story = {
+  name: "Password field behaviour"
+};
